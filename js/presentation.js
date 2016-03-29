9c0548b1cd9f24c9f3f518b39ac0685ffc6598be
JaminQian <jaminqian@outlook.com>
2016-03-29 18:42:18 +0800

    1.0

--- js/presentation.js
+++ js/presentation.js
@@ -1,3 +1,12 @@
 (function(){
     impress().init();
+
+    document.body.onmousewheel = function(event) {
+        event = event || window.event;
+        if(event.deltaY < 0){
+            impress().next();
+        }else if (event.deltaY > 0){
+            impress().prev();
+        }
+    };
 })();
