b05c6a5774152dfa35ea7d923a60bb1e24c8a643
JaminQian <jaminqian@outlook.com>
2016-03-30 21:39:43 +0800

    删除无用文件及格式化代码

--- js/presentation.js
+++ js/presentation.js
@@ -1,11 +1,12 @@
-(function(){
+(function() {
     impress().init();
 
+    // 增加鼠标滑轮切换幻灯片功能
     document.body.onmousewheel = function(event) {
         event = event || window.event;
-        if(event.deltaY < 0){
+        if (event.deltaY < 0) {
             impress().next();
-        }else if (event.deltaY > 0){
+        } else if (event.deltaY > 0) {
             impress().prev();
         }
     };
